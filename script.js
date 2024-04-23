95214c7aeda454b5d576537fc4b4e60ef2131e97
Lscutelx <lscutelx@gmail.com>
2024-04-23 16:46:50 +0300

    create js

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.querySelector('#input-box');
+const listContainer = document.querySelector('#list-container');
+const btn = document.querySelector('.row button');
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert('You must write something!');
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.append(li);
+        let span = document.createElement('span');
+        span.textContent = '\u00d7';
+        li.append(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+btn.onclick = addTask;
+
+listContainer.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML)
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data')
+}
+
+showTask()
